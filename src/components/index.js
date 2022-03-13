05a4424a5279470753c36f0e96c989f132095aa4
Will-Andrade <willian.santos.andra@outlook.com>
2022-03-13 13:08:50 -0400

    feat: creates the Header component

---
+++ src/components/index.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+const Container = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-bottom: .1rem solid #555;
+    padding: 2rem;
+
+    a {
+        text-decoration: none;
+        color: #333;
+        font-size: 2.4rem;
+
+        &:hover {
+            text-decoration: underline;
+            color: #555;
+        }
+    }
+`;
+
+export function Header() {
+    return (
+        <Container>
+            <a href='/'>Sistema de Gerenciamento Escolar</a>
+        </Container>
+    );
+}
