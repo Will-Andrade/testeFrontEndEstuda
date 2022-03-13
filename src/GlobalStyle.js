40a674c7f039fdab340a6bf46db39f4ba3bccc0b
Will-Andrade <willian.santos.andra@outlook.com>
2022-03-13 14:12:28 -0400

    feat: updates global styles

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,6 +1,14 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+    /* 
+        - Font Size System (px)
+        10 / 12 / 14 / 16 / 18 / 20 / 24 / 30 / 36 / 44 / 52 / 62 / 74 / 86 / 98
+
+        - Spacing System (px)
+        2 / 4 / 8 / 12 / 16 / 24 / 32 / 48 / 64 / 80 / 96 / 128
+    */
+    
     * {
         padding: 0;
         margin: 0;
@@ -15,5 +23,6 @@ export const GlobalStyle = createGlobalStyle`
         font-family: 'Roboto', sans-serif;
         font-weight: 400;
         line-height: 1;
+        background-color: #eeeeee;
     }
 `;
