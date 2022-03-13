71fa023bef4f5ec6244a5a521a00d363a4cf4c24
Will-Andrade <willian.santos.andra@outlook.com>
2022-03-13 14:13:46 -0400

    feat: styles the main container

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import { GlobalStyle } from "./GlobalStyle";
-import { Header } from "./components/Header/index";
-import { Card } from "./components/Card";
+import { Header } from "./components/Header";
+import { CardList } from "./components/CardList";
+import styled from 'styled-components';
+
+const Container = styled.main`
+  height: 80vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <Header />
-    <h1>Hello World</h1>
-    <Card />
+      <Container>
+        <CardList />
+      </Container>
     </>
   );
 }
