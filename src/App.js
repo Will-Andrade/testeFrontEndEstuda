be693c9a86fdf56777e1e4c8f36c81bba67fe155
Will-Andrade <willian.santos.andra@outlook.com>
2022-03-13 13:08:22 -0400

    feat: creates the GlobalStyle for the app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import { GlobalStyle } from "./GlobalStyle";
+import { Header } from "./components/index";
+
 function App() {
   return (
+    <>
+      <GlobalStyle />
+      <Header />
     <h1>Hello World</h1>
+    </>
   );
 }
 
